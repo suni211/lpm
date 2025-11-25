68e61ddff98ad91e7752c15a05d8495228638ca1
sun111 <hisamira5284@gmail.com>
2025-11-26 00:59:52 +0900

    fix: PM2를 npm start로 실행하도록 설정 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,17 @@ module.exports = {
   apps: [
     {
       name: 'lpm-server',
-      script: './server/dist/index.js',
+      cwd: './server',
+      script: 'npm',
+      args: 'start',
       instances: 2,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
         PORT: 5000,
       },
-      error_file: './logs/pm2-error.log',
-      out_file: './logs/pm2-out.log',
+      error_file: '../logs/pm2-error.log',
+      out_file: '../logs/pm2-out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       merge_logs: true,
       autorestart: true,
