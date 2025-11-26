96247f4c422e67c0c491a73ed8015be6dea8c91e
sun111 <hisamira5284@gmail.com>
2025-11-26 21:02:40 +0900

    fix: TypeScript import 에러 수정

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
